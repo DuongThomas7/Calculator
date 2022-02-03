9d03c3bb08e52a6a57763de0974eb7d9fbddda18
Thomas D <duongthomas7@gmail.com>
2022-02-03 01:34:39 -0600

    added functionality to all calculator buttons

--- jsMain.js
+++ jsMain.js
@@ -11,17 +11,45 @@ function multiply(first, second){
 }
 
 function divide(first, second){
-    return first/second;
+    return parseFloat(first)/parseFloat(second);
 }
+
+function equal(){
+    switch (userCalculation) {
+        case "divide":
+            return divide(outputOne.textContent, runningTotal.textContent);
+        case "multiply":
+            return multiply(outputOne.textContent, runningTotal.textContent);
+        case "add":
+            return add(outputOne.textContent, runningTotal.textContent);
+        case "subtract":
+            return subtract(outputOne.textContent, runningTotal.textContent);
+    }
+}
+
+let userCalculation = "";
+
 
 const buttons = document.querySelectorAll("button");
 const runningTotal = document.getElementById("output");
+const outputOne = document.getElementById("outputOne");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+        if (runningTotal.textContent.length >= 19) {
+            runningTotal.textContent = runningTotal.textContent.slice(0,19);
+        }
         if (button.classList == "numbers") {
             if (runningTotal.textContent == 0) {
                 runningTotal.textContent = button.textContent;
+            }
+            else if (button.id == "decimal") {
+                if (runningTotal.textContent.includes(".")){
+                    console.log("ERROR - MULTIPLE DECIMALS");
+                }
+                else {
+                    runningTotal.textContent += button.textContent;
+                }
             }
             else {
                 runningTotal.textContent += button.textContent;
@@ -33,13 +61,45 @@ buttons.forEach(button => {
                 runningTotal.textContent = 0;
             }
             else if (button.id == "sign") {
-                console.log(runningTotal.textContent[0]);
                 if (runningTotal.textContent[0] == "-"){
                     runningTotal.textContent = runningTotal.textContent.slice(1);
                 }
                 else {
                     runningTotal.textContent = "-" + runningTotal.textContent;
                 }
+            }
+
+            else {
+                runningTotal.textContent = runningTotal.textContent/100;
+            }
+        }
+
+        else {
+            
+            if (button.id == "divide") {
+                outputOne.textContent = runningTotal.textContent;
+                runningTotal.textContent = 0;
+                userCalculation = "divide";
+            }
+            else if (button.id == "multiply") {
+                outputOne.textContent = runningTotal.textContent;
+                runningTotal.textContent = 0;
+                userCalculation = "multiply";
+            }
+            else if (button.id == "subtract") {
+                outputOne.textContent = runningTotal.textContent;
+                runningTotal.textContent = 0;
+                userCalculation = "subtract";
+            }
+            else if (button.id == "add") {
+                outputOne.textContent = runningTotal.textContent;
+                runningTotal.textContent = 0;
+                userCalculation = "add";
+            }
+            else {
+                let placeHolder = runningTotal.textContent;
+                runningTotal.textContent = equal();
+                outputOne.textContent = placeHolder;
             }
         }
     });
